43810e255438cbf3bda6b94e4c4f98c20a5f7b2b
yihao2000 <91897919+yihao2000@users.noreply.github.com>
2023-02-07 16:50:31 +0700

    thirdpush

--- server/server.go
+++ server/server.go
@@ -8,27 +8,27 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/yihao2000/gqlgen-todos/graph"
+	"github.com/yihao2000/gqlgen-todos/graph/config"
 	"github.com/yihao2000/gqlgen-todos/graph/model"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	dsn := "host=localhost user=postgres password=admin dbname=tpaweb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		panic("Error")
-	}
-
-	db.AutoMigrate(&model.User{})
+	// dsn := "host=localhost user=postgres password=admin dbname=tpaweb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+
+	//Defer Closing Database
+	db := config.GetDB()
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	//Migrate table2 dari Model yang ada
+	db.AutoMigrate(&model.User{})
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: db}}))
 
